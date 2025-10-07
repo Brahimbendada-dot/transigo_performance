9117cb1dc724c4642327685850b54db5ea4dff91
ibrahim bendada <brahimbendada024@gmail.com>
2025-10-07 18:48:41 +0200

    booking test performance

---
+++ bookings/create_booking/average_load.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import http from "k6/http";
+import { check } from "k6";
+
+const baseURL = __ENV.baseURL;
+
+export const APIs = {
+  booking: `${baseURL}/api/v1/booking`,
+};
+
+
+export let options = {
+   stages: [
+    { duration: '2m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+    { duration: '10m', target: 100 }, // stay at 100 users for 30 minutes
+    { duration: '1m', target: 0 }, // ramp-down to 0 users
+  ],
+};
+
+
+// Load files (binary mode)
+const invoice = open("./invoice.PNG", "b");
+const shipment = open("./shipment.PNG", "b");
+
+export default function () {
+  // Send as multipart/form-data
+  const formData = {
+    clientId: "2",
+    date: "2025-10-07 10:30:00",
+    distance: "125.6",
+    locationStartName: "Algiers Central Warehouse",
+    locationDestinationName: "Oran Industrial Zone",
+    locationStartLat: "36.7538",
+    locationStartLong: "3.0588",
+    locationDestinationLat: "35.6971",
+    locationDestinationLong: "-0.6308",
+    description: "Shipment of electronic equipment to Oran branch",
+    capacity: "4.5",
+    invoice: "Yes, I have.",
+    methodepayment: "edahabia",
+    category: "truck",
+    subCategory: "medium truck",
+    responsibleDeliveringName: "tayeb",
+    responsibleReceivingName: "ibrahim",
+    shipmentCapacity: "2.5",
+    timepayment: "Departure",
+    isSharesTrip: 0,
+    
+    // ✅ Attach files using http.file()
+    pictureShipment: http.file(shipment, "shipment.PNG","image/png"),
+    pictureInvoice: http.file(invoice, "invoice.PNG","image/png"),
+  };
+
+  // No need to manually set Content-Type — k6 handles it automatically
+  const response = http.post(APIs.booking, formData);
+
+  check(response, {
+    "status is 201": (r) => r.status === 201,
+    "response status is success": (r) => r.json("status") === "success",
+  });
+}
+
---
+++ bookings/create_booking/stress.js
@@ -0,0 +1,42 @@
+import http from "k6/http";
+import { check } from "k6";
+
+
+
+
+const baseURL=__ENV.baseURL
+
+export const  APIs={
+    login:`${baseURL}/api/v1/authentification/login`,    
+}
+
+export let options = {
+   stages: [
+        { duration: '5m', target: 50 },   // Warm-up
+        { duration: '10m', target: 200 },  // Moderate load
+        { duration: '20m', target: 500 },  // Heavy load
+        // { duration: '30m', target: 1000 }, // Peak stress
+        { duration: '2m', target: 0 },    // Cooldown
+      ],
+};
+
+
+
+export default function () {
+    const body = JSON.stringify({
+        phone: "+213699938225",
+        password: "123456789",
+        phoneToken: "smoke_test_token"
+    });
+
+    const headers = {
+        "Content-Type": "application/json"
+    };
+
+    const response = http.post(APIs.login, body, { headers });
+
+    check(response, {
+        "validate status code": (r) => r.status === 200,
+        "validate response body": (r) => r.json("status") === "success"
+    });
+}
